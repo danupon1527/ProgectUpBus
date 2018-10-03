4b3883d6baba0964da556c7fb2cc4c4f97dc17ea
danupon1527 <danupon.1527@gmail.com>
2018-10-04 01:46:47 +0700

    editmap

--- src/pages/divide-journey/divide-journey.ts
+++ src/pages/divide-journey/divide-journey.ts
@@ -35,6 +35,7 @@ export class DivideJourneyPage {
   }
 
   ionViewDidLoad() {
+    // this.initMap()
     this.showMap()
     // console.log('ionViewDidLoad DivideJourneyPage');
     // this.geolocation.getCurrentPosition().then((resp) => {
@@ -90,5 +91,40 @@ export class DivideJourneyPage {
       }
     })
   }
+  // initMap() {
+  //   var directionsDisplay = new google.maps.DirectionsRenderer;
+  //   var directionsService = new google.maps.DirectionsService;
+  //   var map = new google.maps.Map(document.getElementById('map'), {
+  //     zoom: 7,
+  //     center: {lat: 41.85, lng: -87.65}
+  //   });
+  //   directionsDisplay.setMap(map);
+  //   directionsDisplay.setPanel(document.getElementById('right-panel'));
+
+  //   var control = document.getElementById('floating-panel');
+  //   control.style.display = 'block';
+  //   map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+  //   var onChangeHandler = function(){calculateAndDisplayRoute(directionsService, directionsDisplay);};
+    
+  //   document.getElementById('start').addEventListener('change', onChangeHandler);
+  //   document.getElementById('end').addEventListener('change', onChangeHandler);
+  // }
+
+  // function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  //   var start = document.getElementById('start').value;
+  //   var end = document.getElementById('end').value;
+  //   directionsService.route({
+  //     origin: start,
+  //     destination: end,
+  //     travelMode: 'DRIVING'
+  //   }, function(response, status) {
+  //     if (status === 'OK') {
+  //       directionsDisplay.setDirections(response);
+  //     } else {
+  //       window.alert('Directions request failed due to ' + status);
+  //     }
+  //   });
+  // }
 
 }
